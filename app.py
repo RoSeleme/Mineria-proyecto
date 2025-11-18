c37c2f6eb669955d8789ee15f6f1fd753893f4e6
RoSeleme <110144497+RoSeleme@users.noreply.github.com>
2025-11-18 18:12:42 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title="Siniestros Viales en Argentina", layout="wide")
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("siniestros_limpio.csv")
+    return df
+
+df = load_data()
+
+
+
+# TÍTULO Y DESCRIPCIÓN GENERAL
+st.title(" Siniestros Viales Fatales en Argentina (2017–2023)")
+st.write("Este dashboard forma parte del proyecto final de la materia Minería de Datos.")
+
+st.subheader("Vista general del dataset")
+st.dataframe(df.head())
+ 
